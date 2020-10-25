de55a2385f76603a07ef80dd9d79c9871e3913b6
Katrine <KatsCoding@users.noreply.github.com>
2020-10-26 00:08:09 +0100

    Added first batch of changes to gui

--- src/main/java/com/company/GUI_Controller.java
+++ src/main/java/com/company/GUI_Controller.java
@@ -7,8 +7,13 @@ import gui_main.GUI;
 import java.awt.*;
 
 public class GUI_Controller {
+    private GUI gui;
     void makeGUI() {
         GUI_Field[] fields = {new GUI_Street(), new GUI_Street(), new GUI_Street()};
-        GUI gui = new GUI(fields, Color.gray);
+        gui = new GUI(fields, Color.gray);
+    }
+
+    void rollDice(int d1, int d2) {
+        gui.setDice(d1, d2);
     }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,9 +2,14 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
         var gui = new GUI_Controller();
         gui.makeGUI();
+    // Dette er et forslag til setup med bestemte tal da vi endnu ikke at fået indsat vores dice fra del 1
+        Thread.sleep(1000); // pauser programmet i 1000 milisekunder (basically its fancy af)
+        gui.rollDice(1, 3);
+        Thread.sleep(1000);
+        gui.rollDice(6, 4);
     }
 }
