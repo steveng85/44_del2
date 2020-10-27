41df2026cc5ed98c87562bfadfca30f52c585098
Schnack <simon.schnack99@gmail.com>
2020-10-27 22:47:36 +0100

    Generete the field insted of manuelelly doing it

--- src/main/java/com/company/GUI_Controller.java
+++ src/main/java/com/company/GUI_Controller.java
@@ -2,6 +2,7 @@ package com.company;
 
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
+import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
@@ -9,11 +10,15 @@ import java.awt.*;
 public class GUI_Controller {
     private GUI gui;
     void makeGUI() {
-        GUI_Field[] fields = {new GUI_Street(), new GUI_Street(), new GUI_Street()};
+        GUI_Field[] fields = FieldFactory.makeGUIFields();
+
         gui = new GUI(fields, Color.gray);
     }
 
     void rollDice(int d1, int d2) {
         gui.setDice(d1, d2);
+    }
+    void showMessage(String message){
+        gui.showMessage(message);
     }
 }
