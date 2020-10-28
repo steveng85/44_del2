568f09626b0fd797c9c7a5d0e9b2cc4094d19405
Schnack <simon.schnack99@gmail.com>
2020-10-28 19:01:56 +0100

    fixed something...

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -49,33 +49,30 @@ public class Game {
 
     public int checkRollRules(int t) {
         int rollTotal =  switch (t) {
-            case 2 -> {//+250
-            }
-            case 3 -> {//-100
-            }
-            case 4 -> {//+100
-            }
-            case 5 -> {//-20
-            }
-            case 6 -> {//+180
-            }
-            case 7 -> {//+0
-            }
-            case 8 -> {//-70
-            }
-            case 9 -> {//+60
-            }
-            case 10 -> {//-80
-            }
-            case 11 -> {//-50
-            }
-            case 12 -> {//+650
-            }
+            case 2 -> +250;
 
+            case 3 -> -100;
 
+            case 4 -> +100;
 
+            case 5 -> -20;
 
-        }
+            case 6 -> +180;
+
+            case 7 -> +0;
+
+            case 8 -> -70;
+
+            case 9 -> +60;
+
+            case 10 -> -80;
+
+            case 11 -> -50;
+
+            case 12 -> +650;
+            default -> +0;
+
+        };
         return rollTotal;
     }
 
