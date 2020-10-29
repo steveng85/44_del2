e91ddeb756284e8d4300ac82987c9f3cc9a71c26
Katrine <KatsCoding@users.noreply.github.com>
2020-10-29 22:59:20 +0100

    Cleared test package and created correct place for tests as well as added dependency to pom.xml

---
+++ src/test/java/Game/AccountTest.java
@@ -0,0 +1,24 @@
+package Game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void addCoins() {
+    }
+
+    @Test
+    void reset() {
+    }
+
+    @Test
+    void isFull() {
+    }
+
+    @Test
+    void getCoins() {
+    }
+}
---
+++ src/test/java/Game/DiceTest.java
@@ -0,0 +1,16 @@
+package Game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @Test
+    void roll() {
+    }
+
+    @Test
+    void getEyeValue() {
+    }
+}
