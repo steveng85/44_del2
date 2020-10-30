13533028385a34db18c2f362f0a514aa17309dec
Katrine <KatsCoding@users.noreply.github.com>
2020-10-30 04:55:37 +0100

    made working gui. still need few ui design corrections but fully functional

--- src/main/java/Game/GUI_Game.java
+++ src/main/java/Game/GUI_Game.java
@@ -10,13 +10,43 @@ public class GUI_Game {
     Player currentPlayer;
     boolean gameInProgress = true;
     GUI_Controller gui_controller = new GUI_Controller();
+    Field[] fields = FieldFactory.makeFields(0);
 
     public void startGame() {
         gui_controller.makeGUI();
         player1 = new Player(gui_controller.player1Input());
         player2 = new Player(gui_controller.player2Input());
-        currentPlayer = player1;
+        currentPlayer = player2;
         gui_controller.addPlayers(player1, player2);
+        while (gameInProgress){
+            if (currentPlayer == player1)
+                currentPlayer = player2;
+            else {
+                currentPlayer = player1;
+            }
 
+            gameRound();
+
+        }
+       gui_controller.showMessage("CONGRATULATIONS! " + currentPlayer.getName() + " has reached 3000 coins and won the game ");
+    }
+
+    private void gameRound() {
+        int roll1 = dice1.roll();
+        int roll2 = dice2.roll();
+        gui_controller.rollDice(currentPlayer, roll1, roll2);
+        currentPlayer.addCoins(fields[roll1+roll2].getValue());
+        gui_controller.updateBalance(player1, player2);
+        if (currentPlayer.checkWin()) {
+            gameInProgress = false;
+            return;
+        }
+        if (roll1 == roll2) {
+            if (currentPlayer == player1)
+                currentPlayer = player2;
+            else {
+                currentPlayer = player1;
+            }
+        }
     }
 }
