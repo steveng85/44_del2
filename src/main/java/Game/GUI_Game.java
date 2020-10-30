7a2f87db994f3fec6eb7e0b674b2f5dd4a0f49aa
Katrine <KatsCoding@users.noreply.github.com>
2020-10-30 02:37:59 +0100

    changes to main to decide type of UI

---
+++ src/main/java/Game/GUI_Game.java
@@ -0,0 +1,8 @@
+package Game;
+
+public class GUI_Game {
+    public void startGame() {
+        System.out.println("hello this is gui game!");
+
+    }
+}
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
 import Game.Game;
+import Game.GUI_Game;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Game().startGame();
+        if (args.length == 0){
+            new GUI_Game().startGame();
+        } else {
+          if (args[0].equalsIgnoreCase("gui"))
+              new GUI_Game().startGame();
+          else if (args[0].equalsIgnoreCase("Text"))
+              new Game().startGame();
+          else System.out.println("Not understood. Please type gui for GUI interface or text for text interface.");
+        }
+
+
     }
 }
